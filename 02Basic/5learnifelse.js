45e618e144a4954ed30c6299ebc50be77d0d0f10
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-01-03 13:28:40 +0530

    Create 5learnifelse.js

---
+++ 02Basic/5learnifelse.js
@@ -0,0 +1,56 @@
+/*
+if (true) {
+    console.log("We are inside the if block")
+    console.log("We are STILL inside the if block")
+} else {
+    console.log("We are ouside the if block")
+}*/
+
+// if (false) {
+//     console.log("We are inside the if block")
+// } else if (true) {
+//     console.log("We are inside the CABS if block")
+// } else
+//     console.log("We are outside the if block")
+
+// if (false) {
+//     console.log("We are inside the if block")
+// } else if (false) {
+//     console.log("We are inside the CABS if block")
+// } else
+//     console.log("We are outside the if block")
+
+//LearnCodeOnline
+
+var whoIsHere = 'user'
+
+if (whoIsHere == "user") {
+    console.log("Greetings Message for user.....")
+    console.log("Allow acces to view all courses.")
+} else if (whoIsHere === "teacher") {
+    console.log("Greetings Message for teacher.....")
+    console.log("Allow acces to his courses.")
+} else {
+
+    console.log("MESSAGE : Please Verify your Email")
+    console.log("Send user and email for verification")
+}
+
+// grade problem
+
+// students marks 
+// 10- Amazing
+// 5- Good
+// 3- Poor
+// 0- Fail
+
+// var marks = 10
+s
+// if (marks == 10)
+//     console.log("Amazing")
+// else if (marks == 5)
+//     console.log("Good")
+// else if (marks == 3)
+//     console.log("Poor")
+// else if (marks == 0)
+//     console.log("Fail")
