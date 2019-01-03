c05e68957bc55d788dff8702dccb3d98f614d8c8
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-01-03 13:27:42 +0530

    Create 4nullandbool.js

---
+++ 01Basic/4nullandbool.js
@@ -0,0 +1,16 @@
+var temp
+console.log("Current temperature is : ", temp)
+
+
+// GRADE SYSTEM
+
+// 10-GREAT JOB
+// 5-WORK HARDER
+// 2-POOR
+// 0-FAIL   
+
+// boolean operations are true and false only
+
+let grade = 7
+let myGrade = (grade < 10)
+console.log(myGrade)
