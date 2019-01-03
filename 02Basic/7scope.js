b43a9591c15094edecaaf92c171ac380c065ac85
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-01-03 13:29:40 +0530

    Create 7scope.js

---
+++ 02Basic/7scope.js
@@ -0,0 +1,72 @@
+ // let iAmGlobal = 'someValue'
+
+ // if(true)
+ // {
+ //     let iamlocal = 'Any value'
+ //     console.log(iAmGlobal)
+ //     console.log(iamlocal)
+ // }
+
+ // console.log(iamlocal)
+ // console.log(iAmGlobal)
+
+ // let iAmGlobal = 'someValue'
+
+ // if(true)
+ // {
+ //     let iamlocal = 'Any value'
+ //     console.log(iAmGlobal)
+ //     iAmGlobal='superman'
+ //     console.log(iamlocal)
+ // }
+
+ // console.log(iamlocal)
+ // console.log(iAmGlobal)
+
+ // 'let' when declare inside a function is local variable but for the case of 'var' when declared anywhere in the program may be in any module is defined as a global variable.
+
+ // let iAmGlobal = 'someValue'
+
+ // if (true) {
+ //     var iamlocal = 'Any value'
+ //     console.log(iAmGlobal)
+ //     iAmGlobal = 'superman'
+ //     console.log(iamlocal)
+ // }
+
+ // console.log(iamlocal)
+ // console.log(iAmGlobal)
+
+ // Kings Territory Problem
+
+ // let king = 'John'
+
+ // if (true) {
+ //     let king = 'Sam'
+
+ //     if (true) {
+ //         let king = 'Ram'
+ //         console.log(king)
+ //     }
+ // }
+
+ // if (true) {
+ //     console.log(king)
+ // }
+
+ // -----------------------------------------
+
+ //let king = 'John'
+
+ if (true) {
+     //let king = 'Sam'
+
+     if (true) {
+         king = 'Ram'
+         console.log(king)
+     }
+ }
+
+ if (true) {
+     console.log(king)
+ }
