4eb8638855a06b2040cc40a8b15359f826d97a9e
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-01-03 13:34:19 +0530

    Create 12myObjects.js

---
+++ 12Intermediate/12myObjects.js
@@ -0,0 +1,23 @@
+let myYoutubeVideo = {
+    title: 'loops in JavaScript',
+    videoLength: 15,
+    videoCreator: 'Aditya Prakash',
+    videoDescription: 'this is a video description and long one',
+}
+
+console.log(myYoutubeVideo);
+
+console.log(`Hey new video by title ${myYoutubeVideo.title} created by ${myYoutubeVideo.videoCreator}`);
+
+// assignment
+
+// let course = {
+//     courseName: 'Learn JavaScript',
+//     price: 150,
+//     author: 'Hawkins',
+
+// }
+
+// console.log('===================================================================================');
+// console.log(`Hey there User, We have a New course "${course.courseName}" by ${course.author} at price : ${course.price}Rs`);
+// console.log('===================================================================================');
