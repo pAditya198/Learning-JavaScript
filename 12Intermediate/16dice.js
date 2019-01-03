7fc7fe001cd6e5cc6995f91fbec3ab2dcaeb60cd
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-01-03 13:36:23 +0530

    Create 16dice.js

---
+++ 12Intermediate/16dice.js
@@ -0,0 +1,15 @@
+// let result = 22 / 7
+
+// console.log(result)
+// console.log(result.toFixed(2))
+// console.log(Math.floor(result))
+// console.log(Math.ceil(result))
+
+// console.log(Math.random() * 11)
+
+let upper = 6
+let lower = 1
+
+let randNum = Math.floor(Math.random() * (upper - lower + 1)) + lower
+
+console.log(randNum);
