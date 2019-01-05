5fff059e31310a4cd2a1a0c365b4060e3d810529
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-01-05 16:46:16 +0530

    Create 17equals.js

---
+++ 12Intermediate/17equals.js
@@ -0,0 +1,24 @@
+// console.log(2 == 2)
+// console.log(2 == 2.0)
+// console.log(2 === 2.0)
+
+// let myvar = ''
+// let myvartwo = ''
+
+// console.log(myvar == myvartwo);
+// console.log(myvar === myvartwo);
+
+// let myvar = {}
+// let myvarTwo = {}
+
+// console.log(myvar == myvarTwo);
+// console.log(myvar === myvarTwo);
+
+// let myvar = {}
+// let myvarTwo = myvar
+
+// console.log(myvar == myvarTwo);
+// console.log(myvar === myvarTwo);
+
+// console.log(2 == '2')
+// console.log(2 === '2')
