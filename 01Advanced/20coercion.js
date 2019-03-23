e5f1f0d272b55ae984e9847fd5367f5af51dfc90
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-03-24 00:44:21 +0530

    Create 20coercion.js

---
+++ 01Advanced/20coercion.js
@@ -0,0 +1,42 @@
+// console.log('5' - 5);
+// console.log('5' + 5);
+
+// const giveType = typeof '5'
+// console.log(giveType);
+
+// const adder = true + 5
+// const adder = 5 + false
+// console.log(adder);
+
+const loginDetails = []
+    // login for getting details from DB
+const loginID = loginDetails[0]
+
+if (loginDetails) {
+    console.log('Allow user');
+
+} else {
+    console.log("auth failure");
+
+}
+// if (' ') {
+//     console.log('Allow user');
+
+// } else {
+//     console.log("auth failure");
+
+// }
+// if ('') {
+//     console.log('Allow user');
+
+// } else {
+//     console.log("auth failure");
+
+// }
+
+//  values that interpret as false:
+// false
+// 0
+//     '' - empty string
+// null
+// undefined
